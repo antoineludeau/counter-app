9cb56a8188d17c47476e8c2340a704cebab1a3a4
Antoine LUDEAU <antoineludeau@MacBook-Air-de-Antoine.local>
2022-11-09 17:07:40 +0100

    Added responsive font size theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import {
+  createTheme,
+  responsiveFontSizes,
+  ThemeProvider,
+} from "@mui/material/styles";
+
 import { HomePage } from "./components/pages/home";
 
+let theme = createTheme();
+theme = responsiveFontSizes(theme);
+
 function App() {
-  return <HomePage />;
+  return (
+    <ThemeProvider theme={theme}>
+      <HomePage />;
+    </ThemeProvider>
+  );
 }
 
 export default App;
